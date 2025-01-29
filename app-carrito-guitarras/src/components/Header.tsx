8d8a20445ff364155c45f05335ca8cde567a5146
jcacunainge <jcacunainge@gmail.com>
2025-01-29 11:08:33 -0500

    ref[Subida de proyecto carrito de compras, se hizo una ultima mejora el visualizacion de cantida items en el carrito]

---
+++ app-carrito-guitarras/src/components/Header.tsx
@@ -0,0 +1,122 @@
+import { useMemo, Dispatch } from "react";
+import type { CartItem } from "../types";
+import type { CartActions } from "../reducers/cart-reducer";
+
+type HeaderProps = {
+    cart: CartItem[];
+    dispatch: Dispatch<CartActions>;
+};
+
+export default function Header({ cart, dispatch }: HeaderProps) {
+    // Estado derivado
+    const isEmpty = useMemo(() => cart.length === 0, [cart]);
+    const cartTotal = useMemo(() => cart.reduce((total, item) => total + item.quantity * item.price, 0), [cart]);
+
+    // Número total de elementos en el carrito
+    const cartItemCount = useMemo(() => cart.reduce((total, item) => total + item.quantity, 0), [cart]);
+
+    return (
+        <header className="py-5 header">
+            <div className="container-xl">
+                <div className="row justify-content-center justify-content-md-between">
+                    <div className="col-8 col-md-3">
+                        <a href="index.html">
+                            <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
+                        </a>
+                    </div>
+                    <nav className="col-md-6 mt-5 d-flex align-items-start justify-content-end">
+                        <div className="carrito position-relative">
+                            {/* Icono del carrito */}
+                            <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
+
+                            {/* Indicador de cantidad en el carrito */}
+                            {!isEmpty && (
+                                <span
+                                    className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+                                    style={{ fontSize: "1.4rem", color: "red", padding: "0.5rem 1rem" }}
+                                >
+                                    {cartItemCount}
+                                </span>
+                            )}
+
+                            <div id="carrito" className="bg-white p-3">
+                                {isEmpty ? (
+                                    <p className="text-center">El carrito está vacío</p>
+                                ) : (
+                                    <>
+                                        <table className="w-100 table">
+                                            <thead>
+                                                <tr>
+                                                    <th>Imagen</th>
+                                                    <th>Nombre</th>
+                                                    <th>Precio</th>
+                                                    <th>Cantidad</th>
+                                                    <th></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {cart.map((guitar) => (
+                                                    <tr key={guitar.id}>
+                                                        <td>
+                                                            <img
+                                                                className="img-fluid"
+                                                                src={`/img/${guitar.image}.jpg`}
+                                                                alt="imagen guitarra"
+                                                            />
+                                                        </td>
+                                                        <td>{guitar.name}</td>
+                                                        <td className="fw-bold">${guitar.price}</td>
+                                                        <td className="flex align-items-start gap-4">
+                                                            <button
+                                                                type="button"
+                                                                className="btn btn-dark"
+                                                                onClick={() =>
+                                                                    dispatch({ type: "decrease-quantity", payload: { id: guitar.id } })
+                                                                }
+                                                            >
+                                                                -
+                                                            </button>
+                                                            {guitar.quantity}
+                                                            <button
+                                                                type="button"
+                                                                className="btn btn-dark"
+                                                                onClick={() =>
+                                                                    dispatch({ type: "increase-quantity", payload: { id: guitar.id } })
+                                                                }
+                                                            >
+                                                                +
+                                                            </button>
+                                                        </td>
+                                                        <td>
+                                                            <button
+                                                                className="btn btn-danger"
+                                                                type="button"
+                                                                onClick={() =>
+                                                                    dispatch({ type: "remove-from-cart", payload: { id: guitar.id } })
+                                                                }
+                                                            >
+                                                                X
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+
+                                        <p className="text-end">
+                                            Total pagar: <span className="fw-bold">${cartTotal}</span>
+                                        </p>
+                                    </>
+                                )}
+
+                                <button className="btn btn-dark w-100 mt-3 p-2" onClick={() => dispatch({ type: "clear-cart" })}>
+                                    Vaciar Carrito
+                                </button>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+}
